0e90ca907ba95e670b60ec30956ee075e6f700c1
Tarek <tarek_lamarti@live.fr>
2025-02-02 18:01:53 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['platform.theverge.com',
+    'cdn.vox-cdn.com'],
+   
+  },
+};
+
+module.exports = nextConfig;
