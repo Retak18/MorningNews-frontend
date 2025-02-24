0406fae16ae0de7894443ba9bcf90a046b8a2124
Tarek <tarek_lamarti@live.fr>
2025-02-25 00:33:24 +0100

    update API endpoints to use production server

--- components/Header.js
+++ components/Header.js
@@ -26,7 +26,7 @@ function Header() {
   }, []);
 
   const handleRegister = () => {
-    fetch('http://localhost:3001/users/signup', {
+    fetch('https://morning-news-backend-chi.vercel.app/users/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: signUpUsername, password: signUpPassword }),
@@ -42,7 +42,7 @@ function Header() {
   };
 
   const handleConnection = () => {
-    fetch('http://localhost:3001/users/signin', {
+    fetch('https://morning-news-backend-chi.vercel.app/users/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: signInUsername, password: signInPassword }),
